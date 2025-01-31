970faebea834b3fe08557475b7b84b1ee1e10bd4
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-01-31 12:24:36 +0100

    Adapted AnonKey Auth to be simpler to handle

---
+++ Authentication/TokenService.cs
@@ -0,0 +1,53 @@
+using NookpostBackend.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Globalization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace NookpostBackend.Authentication;
+
+/// <summary>
+/// The service provider for token generation.
+/// Initialized as singleton by the application builder.
+/// </summary>
+public class TokenService
+{
+    /// <summary>
+    /// The time set for access token expiry after it has been issued.
+    /// </summary>
+    public const int AccessTokenExpiryTimeInSeconds = 3600;
+    /// <summary>
+    /// Generate a new token for a user.
+    /// </summary>
+    /// <param name="user">The user to generate the token for.</param>
+    public string GenerateNewToken(User user)
+    {
+        ArgumentNullException.ThrowIfNull(user.Uuid);
+        ArgumentNullException.ThrowIfNull(user);
+        ArgumentNullException.ThrowIfNull(user.Username);
+
+        JwtSecurityTokenHandler tokenHandler = new();
+        byte[] jwtIssuerSigningKey = Configuration.Settings.JwtIssuerSigningKey;
+        long tokenExpiryTimestamp = (long)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalSeconds + AccessTokenExpiryTimeInSeconds;
+        SecurityTokenDescriptor tokenDescriptor = new()
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new (ClaimTypes.Name, user.Username),
+                        new (ClaimTypes.Role, "user"),
+                        new (ClaimTypes.Expiration, System.Convert.ToString(tokenExpiryTimestamp, CultureInfo.InvariantCulture))
+                    }),
+            Expires = DateTime.UtcNow.AddSeconds(AccessTokenExpiryTimeInSeconds),
+            SigningCredentials = new(
+                        new SymmetricSecurityKey(jwtIssuerSigningKey),
+                        SecurityAlgorithms.HmacSha256Signature
+                    )
+        };
+        SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+        string tokenString = tokenHandler.WriteToken(token);
+        return tokenString;
+    }
+
+}
+
---
+++ Configuration/Settings.cs
@@ -0,0 +1,161 @@
+#pragma warning disable CA1707
+using Newtonsoft.Json;
+
+namespace NookpostBackend.Configuration;
+
+// NOTE: This class contains public members that are prefixed with a _ and named in camelCase.
+//       While not beautiful, but is required for deserialization.
+
+/// <summary>
+/// Singleton that holds settings about the server.
+/// Automatically loaded and set up by static constructor.
+/// </summary>
+public class Settings
+{
+    #region setup
+    /// <summary>
+    /// Initializes the settings on first access, and saves them again should they be newly generated.
+    /// </summary>
+    static Settings()
+    {
+        _instance = ReadSettings();
+        SaveSettings();
+    }
+    /// <summary>
+    /// Constructor used for JSON loading, takes all parameters and creates a new settings object.
+    /// </summary>
+    [JsonConstructor]
+    public Settings(byte[] _jwtIssuerSigingKey, string _serverUserPasswordPepper)
+    {
+        this._jwtIssuerSigingKey = _jwtIssuerSigingKey;
+        this._serverUserPasswordPepper = _serverUserPasswordPepper;
+        _instance = this;
+    }
+
+    /// <summary>
+    /// Initialize a new settings object with default values.
+    /// </summary>
+    public Settings()
+    {
+        _jwtIssuerSigingKey = Cryptography.Generators.NewRandomByteArray(JwtSigingKeyLength);
+        _serverUserPasswordPepper = Cryptography.Generators.NewRandomString(ServerUserPasswordPepperLength);
+        _instance = this;
+    }
+
+    /// <summary>
+    /// Instance of the settings class.
+    /// </summary>
+    private static Settings _instance;
+
+    #endregion
+
+    #region constants
+
+    /// <summary>
+    /// The set length for the JWT signing key.
+    /// </summary>
+    public const int JwtSigingKeyLength = 256;
+    /// <summary>
+    /// The length of the pepper applied to user passwords.
+    /// </summary>
+    public const int ServerUserPasswordPepperLength = 32;
+    /// <summary>
+    /// The length of the salt applied to user passwords.
+    /// </summary>
+    public const int UserPasswordSaltLength = 32;
+
+
+    #endregion
+
+    #region settings
+
+    /// <summary>
+    /// The key used for signing json web tokens.
+    /// </summary>
+    public static byte[] JwtIssuerSigningKey { get => _instance._jwtIssuerSigingKey; private set => _instance._jwtIssuerSigingKey = value; }
+    /// <summary>
+    /// The background property for the jwt signing key.
+    /// Should not be directly accessed!
+    /// Use <c cref="JwtIssuerSigningKey">Settings.JwtIssuerSigningKey</c> instead!
+    /// </summary>
+    public byte[] _jwtIssuerSigingKey { get; private set; }
+
+    /// <summary>
+    /// The pepper applied to user passwords universally on this server.
+    /// </summary>
+    public static string ServerUserPasswordPepper { get => _instance._serverUserPasswordPepper; set => _instance._serverUserPasswordPepper = value; }
+    /// <summary>
+    /// The pepper applied to user passwords universally on this server.
+    /// Should not be directly accessed!
+    /// Use <c cref="ServerUserPasswordPepper">Settings.ServerUserPasswordPepper</c> instead!
+    /// </summary>
+    public string _serverUserPasswordPepper { get; private set; }
+
+
+    #endregion
+    #region actions
+
+    /// <summary>
+    /// Read the settings from the settings.json file and place them into the singleton instance variable.
+    /// Create a default instance if the file can't be read.
+    /// </summary>
+    public static void LoadSettings()
+    {
+        _instance = ReadSettings();
+    }
+
+    /// <summary>
+    /// Read the settings from the settings.json file.
+    /// Create a default instance if the file can't be read.
+    /// </summary>
+    private static Settings ReadSettings()
+    {
+        Settings? settingsObject;
+        if (System.IO.File.Exists("settings.json"))
+        {
+            using (System.IO.StreamReader streamReader = new System.IO.StreamReader("settings.json"))
+            {
+                try
+                {
+                    string fileContent = streamReader.ReadToEnd();
+                    settingsObject = JsonConvert.DeserializeObject<Settings>(fileContent);
+                    if (settingsObject is null) return new();
+                    return isSettingsIntegrityOk(settingsObject) ? new() : settingsObject;
+                }
+                catch (Newtonsoft.Json.JsonException)
+                {
+                    settingsObject = new();
+                }
+            }
+        }
+        else
+        {
+            settingsObject = new();
+        }
+        return settingsObject;
+    }
+
+    /// <summary>
+    /// Save settings to the settings.json file.
+    /// </summary>
+    public static void SaveSettings()
+    {
+        using (System.IO.StreamWriter streamWriter = new("settings.json"))
+        {
+            string jsonString = JsonConvert.SerializeObject(_instance);
+            streamWriter.Write(jsonString);
+        }
+    }
+
+    private static bool isSettingsIntegrityOk(Settings settings)
+    {
+        return !(
+            settings._jwtIssuerSigingKey != null &&
+            settings._jwtIssuerSigingKey.Length == JwtSigingKeyLength &&
+            settings._serverUserPasswordPepper != null &&
+            settings._serverUserPasswordPepper.Length == ServerUserPasswordPepperLength
+            );
+
+    }
+    #endregion
+}
