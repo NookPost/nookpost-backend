328268167c8653ad527e66fa20db85b596041850
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-01-31 13:11:57 +0100

    Added global usings

---
+++ GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Http.HttpResults;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using System.Security.Claims;
