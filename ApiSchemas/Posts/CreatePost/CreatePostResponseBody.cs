17d06ababb3ffc435aee98e72a4c275068b6f89a
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-04 20:37:41 +0100

    formatted api schemas

--- ApiSchemas/Posts/CreatePost/CreatePostResponseBody.cs
+++ ApiSchemas/Posts/CreatePost/CreatePostResponseBody.cs
@@ -3,10 +3,10 @@ namespace NookpostBackend.ApiSchemas.Posts.CreatePost;
 /// <summary>
 /// The response to a post create message
 /// </summary>
-public sealed class CreatePostResponseBody 
+public sealed class CreatePostResponseBody
 {
     /// <summary>
     /// The UUID of the created post.
     /// </summary>
-    public string? Uuid {get; set;}
+    public string? Uuid { get; set; }
 }
