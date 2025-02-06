f1086d8d32418b83c83a7ba72a3cff022c28ac85
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-02-06 19:06:11 +0100

    Added pagination to filtering posts

--- ApiEndpoints/Posts/GetPostFiltered.cs
+++ ApiEndpoints/Posts/GetPostFiltered.cs
@@ -5,22 +5,32 @@ static class GetPostFiltered
     /// <summary>
     /// Gets a filtered set of posts
     /// </summary>
+    /// <remarks>The list will be ordered by date of creation</remarks>
+    /// <param name="uuid">The UUID of the post to get</param>
+    /// <param name="categoryUuid">The uuid of the category the posts are in</param>
+    /// <param name="page">The page the posts should be on (requires pageItemCount)</param>
+    /// <param name="pageItemCount">The number of posts per page (requires page)</param>
+    /// <param name="databaseHandle">The database handle</param>
     public static Microsoft.AspNetCore.Http.HttpResults.Results<
             Ok<NookpostBackend.ApiSchemas.Posts.GetPostFiltered.GetPostFilteredResponseBody>,
-            BadRequest> HandleRequest(string? uuid, string? categoryUuid, int? page, int? maxCount, NookpostBackend.Data.DatabaseHandle databaseHandle)
+            BadRequest> HandleRequest(string? uuid, string? categoryUuid, int? page, int? pageItemCount, NookpostBackend.Data.DatabaseHandle databaseHandle)
     {
-        System.Console.WriteLine(uuid is null);
-        System.Console.WriteLine(categoryUuid is null);
         databaseHandle.Database.EnsureCreated();
 
-        List<Models.Post> filteredPosts = databaseHandle.Posts.Where(p =>
+        IQueryable<Models.Post> filteredPosts = databaseHandle.Posts.Where(p =>
                     ((categoryUuid == null) || p.CategoryUuid == categoryUuid) &&
                     ((uuid == null) || p.Uuid == uuid)
-                ).ToList();
+                ).OrderByDescending(p => p.CreatedOn);
+
+        if ((!(page is null)) && (!(pageItemCount is null)))
+        {
+            int start = (int)(pageItemCount * page);
+            filteredPosts = filteredPosts.Skip(start).Take((int)pageItemCount);
+        }
 
         List<NookpostBackend.ApiSchemas.Posts.GetPostFiltered.GetPostFilteredPost> returnedObjects = new();
 
-        foreach (Models.Post post in filteredPosts)
+        foreach (Models.Post post in filteredPosts.ToList())
         {
             returnedObjects.Add(new()
             {
